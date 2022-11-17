2090f5dfb28ca439c27f29bc569c7545fb0b65d1
Matt Whisenhunt <matthewwhisenhunt@outlook.com>
2022-11-17 16:36:24 -0700

    Cludg

--- main.go
+++ main.go
@@ -10,15 +10,55 @@ import (
 var port = ":4040"
 
 func main() {
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	http.HandleFunc("/books/", booooooks)
+	http.HandleFunc(COLLECTION_ROOT, booooooks)
 
 	go http.ListenAndServe(port, nil)
 	<-c
 }
 
+const COLLECTION_ROOT = "/books/"
+
 func booooooks(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == COLLECTION_ROOT {
+		if r.Method == http.MethodPost {
+			// CREATE
+
+		} else {
+			// LIST
+
+		}
+
+	} else {
+		// bookId := r.URL.Path[len(COLLECTION_ROOT):]
+		switch r.Method {
+		case http.MethodPut, http.MethodPatch:
+			// UPDATE
+		case http.MethodDelete:
+			// DELETE
+		case http.MethodGet:
+			// GET
+		default:
+
+		}
+	}
+
 	w.Write([]byte("Sup"))
+}
+
+type Book struct {
+	Title  string
+	Author string
+
+	Abridged bool
+	Edition  string
+	ISBN     string
+
+	CopiesAvailable uint
+
+	Id    uint64
+	URLid string
 }
