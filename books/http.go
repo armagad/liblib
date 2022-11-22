1b4b5b876d793f818ea842cc8db09996a3c945c0
Matt Whisenhunt <mattwhisenhunt@gmail.com>
2022-11-22 13:58:02 -0700

    Organize books api a bit

--- books/http.go
+++ books/http.go
@@ -13,106 +13,20 @@ func (api *BooksApi) Handler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == api.CollectionRoot {
 		switch r.Method {
 		case http.MethodPost:
-			// CREATE
-			book, err := ReadBodyAsBook(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusUnsupportedMediaType)
-				w.Write([]byte(err.Error()))
-				return
-			}
-			book.Id = api.badkey
-			book.URLid = BookId(fmt.Sprintf("%d", book.Id))
-			api.badkey++
-
-			// -- db
-			go search.AddItems(book)
-			api.libraryLock.Lock()
-			api.library[book.URLid] = book
-			api.libraryLock.Unlock()
+			api.HttpCreate(w, r)
 		case http.MethodGet:
-			// LIST
-			c := make(chan Book)
-
-			// -- db
-			go func() {
-				api.libraryLock.RLock()
-				for _, book := range api.library {
-					c <- book
-				}
-				api.libraryLock.RUnlock()
-				close(c)
-			}()
-
-			list := []Book{}
-			for book := range c {
-				list = append(list, book)
-			}
-			b, err := json.Marshal(list)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte(err.Error()))
-				return
-			}
-			w.Write(b)
+			api.HttpList(w, r)
 		default:
 			w.WriteHeader(http.StatusNotFound)
 		}
-
 	} else {
-		bookId := BookId(r.URL.Path[len(api.CollectionRoot):])
 		switch r.Method {
 		case http.MethodPut, http.MethodPatch:
-			// UPDATE
-			upbook, err := ReadBodyAsBook(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusUnsupportedMediaType)
-				w.Write([]byte(err.Error()))
-				return
-			}
-			if book, ok := api.library[bookId]; ok {
-				if upbook.Title != "" {
-					book.Title = upbook.Title
-				}
-				if upbook.Author != "" {
-					book.Author = upbook.Author
-				}
-				if book.Abridged {
-					book.Abridged = upbook.Abridged
-				}
-
-				api.libraryLock.Lock()
-				api.library[bookId] = book
-				api.libraryLock.Unlock()
-			}
+			api.HttpUpdate(w, r)
 		case http.MethodDelete:
-			// DELETE
-			api.libraryLock.RLock()
-			book, ok := api.library[bookId]
-			api.libraryLock.RUnlock()
-			if !ok {
-				w.WriteHeader(http.StatusNotFound)
-				return
-			}
-			go search.PurgeItems(book)
-			api.libraryLock.Lock()
-			delete(api.library, bookId)
-			api.libraryLock.Unlock()
+			api.HttpDelete(w, r)
 		case http.MethodGet:
-			// GET
-			api.libraryLock.RLock()
-			book, ok := api.library[bookId]
-			api.libraryLock.RUnlock()
-			if ok {
-				b, err := json.Marshal(book)
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					w.Write([]byte(err.Error()))
-					return
-				}
-				w.Write(b)
-				return
-			}
-			w.WriteHeader(http.StatusNotFound)
+			api.HttpGet(w, r)
 		default:
 			w.WriteHeader(http.StatusNotFound)
 		}
@@ -127,4 +41,106 @@ func ReadBodyAsBook(body io.ReadCloser) (book Book, err error) {
 	}
 	err = json.Unmarshal(b, &book)
 	return
+}
+
+func (api *BooksApi) HttpCreate(w http.ResponseWriter, r *http.Request) {
+	book, err := ReadBodyAsBook(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	book.Id = api.badkey
+	book.URLid = BookId(fmt.Sprintf("%d", book.Id))
+	api.badkey++
+
+	// -- db
+	go search.AddItems(book)
+	api.libraryLock.Lock()
+	api.library[book.URLid] = book
+	api.libraryLock.Unlock()
+}
+
+func (api *BooksApi) HttpList(w http.ResponseWriter, r *http.Request) {
+	c := make(chan Book)
+
+	// -- db
+	go func() {
+		api.libraryLock.RLock()
+		for _, book := range api.library {
+			c <- book
+		}
+		api.libraryLock.RUnlock()
+		close(c)
+	}()
+
+	list := []Book{}
+	for book := range c {
+		list = append(list, book)
+	}
+	b, err := json.Marshal(list)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write(b)
+}
+
+func (api *BooksApi) HttpUpdate(w http.ResponseWriter, r *http.Request) {
+	bookId := BookId(r.URL.Path[len(api.CollectionRoot):])
+	upbook, err := ReadBodyAsBook(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	if book, ok := api.library[bookId]; ok {
+		if upbook.Title != "" {
+			book.Title = upbook.Title
+		}
+		if upbook.Author != "" {
+			book.Author = upbook.Author
+		}
+		if book.Abridged {
+			book.Abridged = upbook.Abridged
+		}
+
+		api.libraryLock.Lock()
+		api.library[bookId] = book
+		api.libraryLock.Unlock()
+	}
+}
+
+func (api *BooksApi) HttpDelete(w http.ResponseWriter, r *http.Request) {
+	bookId := BookId(r.URL.Path[len(api.CollectionRoot):])
+	api.libraryLock.RLock()
+	book, ok := api.library[bookId]
+	api.libraryLock.RUnlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	go search.PurgeItems(book)
+	api.libraryLock.Lock()
+	delete(api.library, bookId)
+	api.libraryLock.Unlock()
+}
+
+func (api *BooksApi) HttpGet(w http.ResponseWriter, r *http.Request) {
+	bookId := BookId(r.URL.Path[len(api.CollectionRoot):])
+	api.libraryLock.RLock()
+	book, ok := api.library[bookId]
+	api.libraryLock.RUnlock()
+	if ok {
+		b, err := json.Marshal(book)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write(b)
+		return
+	}
+	w.WriteHeader(http.StatusNotFound)
 }
